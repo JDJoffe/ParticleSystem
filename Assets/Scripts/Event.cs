62c0e90645d7148a5809cd63cca11896f69f6dc4
josh <48034855+JDJoffe@users.noreply.github.com>
2019-11-18 09:17:47 +1100

    yep

---
+++ Assets/Scripts/Event.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public class Event : MonoBehaviour
+{
+  public  GameObject hostObject;
+  public  ParticleSystem particleSystem;
+    public virtual void EventFunc()
+    {
+        particleSystem.Play();
+    }
+
+}
---
+++ Assets/Scripts/ParticleEvents.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleEvents : Event
+{
+    public float duration;
+    ParticleSystem temp;
+    Event eventSystem;
+    // Start is called before the first frame update
+    void Start()
+    {
+        hostObject = gameObject;
+        particleSystem = GetComponent<ParticleSystem>();
+    }
+    public override void EventFunc()
+    {
+        var main = particleSystem.main;
+        main.duration = duration;
+        base.EventFunc();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

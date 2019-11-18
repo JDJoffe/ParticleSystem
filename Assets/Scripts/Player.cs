9810ca982c1c931f316008a5ab117d546e0afdc8
josh <48034855+JDJoffe@users.noreply.github.com>
2019-11-19 10:42:49 +1100

    arrows & player controls

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,63 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+[RequireComponent(typeof(CharacterController))]
 public class Player : MonoBehaviour
 {
+
+    public GameObject arrow;
+    CharacterController character;
+    public Camera headCam;
+    public float speed = 6;
+    public float gravity = 9.8f;
+    public float sensitivityHor = 9.0f;
+    public float sensitivityVert = 9.0f;
+    public float minimumVert = -45.0f;
+    public float maximumVert = 45.0f;
+    private float rotationVert = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        character = GetComponent<CharacterController>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        movement();
+        rotation();
+        if (Input.GetMouseButtonDown(0))
+        {
+            shoot();
+        }
+    }
+    public void movement()
+    {
+        if (Input.GetKey(KeyCode.LeftShift)) { speed = 9f; }
+        else { speed = 6; }
+        float deltaX = Input.GetAxis("Horizontal") * speed;
+        float deltaZ = Input.GetAxis("Vertical") * speed;
+        Vector3 movement = new Vector3(deltaX, 0, deltaZ);
+        movement = Vector3.ClampMagnitude(movement, speed);
+        movement.y = -gravity;
+        movement *= Time.deltaTime;
+        movement = transform.TransformDirection(movement);
+        character.Move(movement);
+    }
+    public void rotation()
+    {
+        transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityHor, 0);
+        rotationVert -= Input.GetAxis("Mouse Y") * sensitivityVert;
+        rotationVert = Mathf.Clamp(rotationVert, minimumVert, maximumVert);
+        headCam.transform.localEulerAngles = new Vector3(rotationVert, headCam.transform.localEulerAngles.y, 0);
+    }
+    public void shoot()
+    {
+        Rigidbody rb;
+        GameObject clone = Instantiate(arrow, transform.position + Vector3.forward, transform.rotation);
+        rb = clone.GetComponent<Rigidbody>();
+        rb.AddForce(clone.transform.forward * 1000f);
+        clone.transform.LookAt(transform.position);
     }
 }
---
+++ Assets/arrow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class arrow : MonoBehaviour
+{
+   public GameObject player;
+    Rigidbody rb;
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        rb = GetComponent<Rigidbody>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 arrowrot = new Vector3(0, rb.velocity.y, 0);
+        transform.LookAt(player.transform.position +arrowrot);
+    }
+}

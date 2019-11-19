a4efc172add0d5c6749227b15e2051421ddeb7a9
josh <48034855+JDJoffe@users.noreply.github.com>
2019-11-19 11:55:23 +1100

    adjustments

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -19,7 +19,8 @@ public class Player : MonoBehaviour
     void Start()
     {
         character = GetComponent<CharacterController>();
-
+       Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame

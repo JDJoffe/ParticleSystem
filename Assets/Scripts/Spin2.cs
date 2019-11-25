b1016fa7439887c2fa5a410af3d239dcfc279371
josh <48034855+JDJoffe@users.noreply.github.com>
2019-11-25 11:34:36 +1100

    shitty tornado

---
+++ Assets/Scripts/Spin2.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin2 : MonoBehaviour
+{
+    public GameObject obj;
+	public float speed;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        obj = this.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		    
+    obj.transform.Rotate(0,speed * Time.deltaTime,0);
+
+    }
+}
